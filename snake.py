7b8a15967c7c7f008d9082ebbbfbaac59471f996
Théo <theo.dasilvapereira@gmail.com>
2025-05-23 22:35:16 +0200

    couleur de la tête différente du corps

--- snake.py
+++ snake.py
@@ -9,7 +9,7 @@ grass_color1 = (170, 215, 81)
 grass_color2 = (162, 209, 73)
 
 head_color = (0, 200, 0)
-body_color = (0, 150, 0)
+body_color = (0, 175, 0)
 
 snake = [(7, 7), (6, 7), (5, 7)]
 direction = (1, 0)
@@ -53,9 +53,6 @@ while running:
     snake.insert(0, new_head)
     snake.pop()
 
-    for (x, y) in snake:
-        rect = pygame.Rect(x * cell_size, y * cell_size, cell_size, cell_size)
-        pygame.draw.rect(screen, (0, 100, 0), rect)
 
     if not apple_exists:
         while True:
@@ -68,6 +65,16 @@ while running:
     
     rect = pygame.Rect(apple_position[0] * cell_size, apple_position[1] * cell_size, cell_size, cell_size)
     pygame.draw.rect(screen, (255, 0, 0), rect)
+
+    cpt = 0
+    for (x, y) in snake:
+        if cpt == 0:
+            color = head_color
+        else:
+            color = body_color
+        rect = pygame.Rect(x * cell_size, y * cell_size, cell_size, cell_size)
+        pygame.draw.rect(screen, color, rect)
+        cpt += 1
 
     if snake[0] == apple_position:
         snake.append(snake[-1])
